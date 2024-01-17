9010d60a27ddf4ace8a6eb4ef14a7ba8058007d9
Adrien Miko <mikolajczykadrien@gmail.com>
2024-01-17 20:02:58 +0100

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const counterDisplay = document.querySelector("h1");
+let counter = 0;
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+  //bubble size
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+  // bubble basic position
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+  // bubble direction
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+  //bubble click event, increment counter and remove bubble
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+  // bubble remove after 8s
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+//bubbleMaker
+setInterval(bubbleMaker, 1000);
